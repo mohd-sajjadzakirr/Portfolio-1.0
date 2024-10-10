c7ae1945891bc7291a4b56291f6f0696dcb78abb
MOHAMMAD SAJJAD ZAKIR <sajjadkhantu@gmail.com>
2024-10-11 01:01:24 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -14,5 +14,5 @@ export const personalData = {
   stackOverflow: '', // Add Stack Overflow link if applicable.
   leetcode: 'https://leetcode.com/u/mohd_sajjadzakirr/',
   devUsername: 'mohdsajjadzakirr',
-  resume: "https://drive.google.com/file/d/1u_safXREoXcyJdXAP_ZjvRze3dz8Ls5F/view?usp=drive_link"
+  resume: "https://drive.google.com/file/d/1sUlou7Kn4Nq3oCrYIwYaO5wLaB02-2vH/view?usp=sharing"
 };
